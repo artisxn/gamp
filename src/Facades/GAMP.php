a72850cd4957d610a6b34e24701474b86481aa49
Codica Studio <CodicaStudioDev@gmail.com>
2020-08-20 02:21:21 -0500

    Initial commit.

---
+++ src/Facades/GAMP.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace codicastudio\LaravelGAMP\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+class GAMP extends Facade
+{
+    /**
+     * Get the registered name of the component.
+     *
+     * @return string
+     */
+    protected static function getFacadeAccessor()
+    {
+        return 'gamp';
+    }
+}
